55e665cee1be099cb02daf106ac0dd0174d64ec1
student <mjeraeunice@gmail.com>
2023-03-09 08:52:28 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+import kotlin.math.min
+
+fun main() {
+    var names=fullName("Mary","Juane")
+    var calculator=
+
+}
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+fun fullName(name1: String,name2: String):Int{
+    var x= name1.length
+    var y= name2.length
+    if(x.equals(y)){
+        println(x)
+    }
+    else{
+        println(y)
+    }
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun numbers(num1:Int,num2:Int,num3:Int){
+    val x=
+}
+
+
+//Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output.
+fun fruit(x:String){
+    var x= "Mango"
+    var y= x.split
+}
+
+//Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+fun Cities(){
+    var cities= arrayOf("Mombasa")
+    var country= cities.plus("Mombasa","Kisumu")
+}
+
+
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+class calculator(addition:Int,Subtraction:Int,Divion:Int,Multiplication:Int){
+    fun addition(){
+
+    }
+
+
+}
